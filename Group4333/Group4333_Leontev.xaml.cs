c3f4b58aa21dd02b5002169b57a295b19b6faa6d
George <egorleontev54@gmail.com>
2026-03-17 14:16:53 +0300

    fear: Leontev Egor 4333

---
+++ Group4333/Group4333_Leontev.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows;
+
+namespace Group4333
+{
+    public partial class Group4333_Leontev : Window
+    {
+        public Group4333_Leontev()
+        {
+            InitializeComponent();
+        }
+    }
+}
